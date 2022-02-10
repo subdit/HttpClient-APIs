e1b84add989d8bc5a8d01dfb0c474ef3c470aab6
Kasidit Bualoeng <subdit@gmail.com>
2022-02-09 21:44:34 -0500

    update handle error in database and data.service.ts file

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 // Handle the Errors
 import { throwError } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER = 'http://localhost:3000/products';
 
   constructor(private httpClient: HttpClient) {}
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unkown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      //Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
   public sendGetRequest() {
-    return this.httpClient.get(this.REST_API_SERVER);
+    return this.httpClient
+      .get(this.REST_API_SERVER)
+      .pipe(catchError(this.handleError));
   }
 }
